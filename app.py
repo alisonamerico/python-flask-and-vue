d7ae10f051a764d38a5ca22994ba05c2b4083ab6
alisonamerico <alison.americo@gmail.com>
2018-10-05 23:46:00 -0300

    template added
    close #3

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-# importing Flask
-<<<<<<< HEAD
-from flask import Flask
-=======
+# importing Flask e render_template
 from flask import Flask, render_template
->>>>>>> template added
 
 # creating instance of Flask
 app = Flask(__name__)
@@ -12,21 +8,14 @@ app = Flask(__name__)
 # creating a route to main page and displaying the message 'Hello World'
 @app.route('/')
 def index():
-<<<<<<< HEAD
-    return 'Hello World!'
-=======
     return render_template('templates/index.html')
->>>>>>> template added
 
 
 # creating a route to page about
 @app.route('/about')
 def about():
     return 'Hi my name is Alison!'
-<<<<<<< HEAD
-=======
 
 
 if __name__ == "__main__":
     app.run()
->>>>>>> template added
