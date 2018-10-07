234a40d4f2f3dfff557f3c95e2b420bf07846de3
alisonamerico <alison.americo@gmail.com>
2018-10-06 22:59:06 -0300

    Creaded a Restful Service and Calling it with Vue
    close #11

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # importing Flask e render_template
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 
 # creating instance of Flask
 app = Flask(__name__)
@@ -17,10 +17,17 @@ def about():
     return render_template('about.html')
 
 
-# creating a route to page about
+# creating a route to page contact
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+# creating a route to page test
+@app.route('/test')
+def test():
+    my_dict = {"title": "Bayside", "genre": "Alternative"}
+    return json.dumps(my_dict)
 
 
 if __name__ == "__main__":
